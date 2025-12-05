47c93000b563044f0eb65bd67392aac14c642bf0
Kolala <kolala08@outlook.fr>
2025-12-05 09:22:16 +0100

    merge wsinter.py

--- src/libs/wsinter.py
+++ src/libs/wsinter.py
@@ -94,22 +94,13 @@ function faire(o){
             {
                 elem[attr]=data[attr];
             }
-            document.body.appendChild(elem);
-        }
-        else
-        {
-            for (attr in data)
+            let parent = document.getElementById(dico["parent_id"]);
+            if ((parent == null) || (parent == undefined))
             {
-                if (attr!="style")
-                {
-                    elem[attr]=data[attr];
-                }
+                parent = document.body;
             }
-        }
-        for (sattr in data["style"])
-        {
-            elem.style[sattr] = data["style"][sattr]; 
-        }
+            parent.appendChild(elem);
+        }        
     }
 }
 
@@ -662,21 +653,28 @@ function faire(o){
         if attr != {}:
             self._push([{"id":id_objet,"data":attr}])
         if style != {}:
-            self._push([{"id":id_objet,"data":{"style":style}}])        
+            self._push([{"id":id_objet,"type":"style","data":{"style":style}}])
+            
+    def remove(self,id_objet):
+        self._push([{"id":id_objet,"type":"delete"}])
+    
+    def inner_text(self,id_objet:str,inner_text:str):
+        self._push([{"id":id_objet,"type":"content","data":inner_text}])
 
-    def insere(self, id_objet:str, balise: str,attr:dict={},style:dict={}):
+    def insere(self, id_objet:str, balise: str,attr:dict={},style:dict={},parent:str="body"):
         """
-        Insère un élément sur la page, dans <body>
+        Insère un élément sur la page, dans l'élément dont l'id est spécifié par parent
 
         Paramètres:
           - id_objet: attribut id de l'objet créé
           - balise: balise de l'objet créé
           - attr: dictionnaire attribut:valeur définissant les atributs de l'objet créé
           - style: dictionnaire attrobut:valeur définissant les attributs de style de l'objet créé
-              
+          - parent: id de l'objet où insérer l'élément créé (default: body)
+          
         Valeur renvoyée : None
         """
-        self._push([{'id':'_nobj','tagName':balise,'data':{'id':id_objet}}])
+        self._push([{'id':'_nobj','type':'create','tagName':balise,'parent_id':parent,'data':{'id':id_objet}}])
         if attr != {}:
             self._push([{"id":id_objet,"data":attr}])
         if style != {}:
