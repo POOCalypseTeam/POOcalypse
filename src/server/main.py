31dde9b7dd57df2ded05cad180cb4573c0692a0c
K0LALA <kolala08@outlook.fr>
2026-02-17 23:18:54 +0100

    un seul appel load_all

--- src/server/main.py
+++ src/server/main.py
@@ -13,6 +13,9 @@ from characters.npc import Interactable, Npc
 from inputs.keyboard import Keyboard
 from inputs.mouse import Mouse
 
+from cProfile import Profile
+from pstats import SortKey, Stats
+
 game = None
 
 def main():
@@ -54,7 +57,6 @@ class Game:
         self.web_manager.attributs(self.player.id, style={"z-index": 10})
 
         self.board = graphics.board.Board(self.web_helper, "spawn")
-        self.board.load_all()
         
         # TODO: Gérer les NPC avec les tiles, et les ajouter au fil qu'on se rapproche pour pas avoir tous les NPC ici du monde H24
         # On crée une lste de NPC pour pouvoir en gérer plusieurs plus facilement
@@ -98,46 +100,54 @@ class Game:
         Pour la physique on pourrait meme viser 30 iterations par seconde
         
         Ainsi on conditionne le temps
-        """        
-        self.do_loop = True
-        last_loop_time = time.time()
-        
-        while self.do_loop:
-            delta_time = time.time() - last_loop_time
-            # 1 / 60 ~= 0.017, on s'embete pas à faire le calcul tout le temps, on pourrait limite stocker la duree dans une variable mais pas tres utile non plus
-            if delta_time < 0.017:
-                continue
-            
-            keys = self.keyboard_manager.get_keys()
-            
-            # On actualise la liste des ennemis en supprimant ceux qui sont morts
-            for enemy in self.enemies:
-                if enemy.is_dead():
-                    self.enemies.remove(enemy)
-
-            # Toutes les instructions ici sont mises en pauses lorsqu'un menu est ouvert par le joueur            
-            if self.interactable is None or not self.interactable.is_opened():
-                in_range_enemies = []
-                player_range = self.player.weapon.range
-                for enemy in self.enemies:
-                    enemy.update(delta_time, self.player)
-                    dst = sqrt((enemy.x - self.player.x) ** 2 + (enemy.y - self.player.y) ** 2)
-                    if dst <= player_range:
-                        in_range_enemies.append(enemy)
-                if not self.player.is_dead():
-                    player_movement = self.player.update(delta_time, keys, in_range_enemies)
-                    self.board.translate(player_movement)
-            
-            self.interactable = None
-            for npc in self.npc:
-                if npc.within_distance(self.player.get_position()):
-                    self.interactable = npc
-                    self.web_manager.inner_text("action-bar", "Appuyez sur E pour interagir")
-                    
-            if self.interactable == None:
-                self.web_manager.inner_text("action-bar", "")
-                
+        """
+        with Profile() as profile:
+            self.do_loop = True
             last_loop_time = time.time()
+            
+            while self.do_loop:
+                delta_time = time.time() - last_loop_time
+                # 1 / 60 ~= 0.017, on s'embete pas à faire le calcul tout le temps, on pourrait limite stocker la duree dans une variable mais pas tres utile non plus
+                if delta_time < 0.017:
+                    continue
+                
+                keys = self.keyboard_manager.get_keys()
+                
+                # On actualise la liste des ennemis en supprimant ceux qui sont morts
+                for enemy in self.enemies:
+                    if enemy.is_dead():
+                        self.enemies.remove(enemy)
+
+                # Toutes les instructions ici sont mises en pauses lorsqu'un menu est ouvert par le joueur            
+                if self.interactable is None or not self.interactable.is_opened():
+                    in_range_enemies = []
+                    player_range = self.player.weapon.range
+                    for enemy in self.enemies:
+                        enemy.update(delta_time, self.player)
+                        dst = sqrt((enemy.x - self.player.x) ** 2 + (enemy.y - self.player.y) ** 2)
+                        if dst <= player_range:
+                            in_range_enemies.append(enemy)
+                    if not self.player.is_dead():
+                        player_movement = self.player.update(delta_time, keys, in_range_enemies)
+                        self.board.translate(player_movement)
+                
+                self.interactable = None
+                for npc in self.npc:
+                    if npc.within_distance(self.player.get_position()):
+                        self.interactable = npc
+                        self.web_manager.inner_text("action-bar", "Appuyez sur E pour interagir")
+                        
+                if self.interactable == None:
+                    self.web_manager.inner_text("action-bar", "")
+                    
+                last_loop_time = time.time()
+            
+            (
+                Stats(profile)
+                .strip_dirs()
+                .sort_stats(SortKey.CUMULATIVE)
+                .print_stats()
+            )
 
     def stop(self):
         """
